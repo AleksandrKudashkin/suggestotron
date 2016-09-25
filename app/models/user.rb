72f380516a49100d5aca1cff7f0e423f83a030ba
akudashkin <akudashkin@yandex.ru>
2016-09-25 18:20:43 -0400

    Show cancel vote if voted

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
   def voted_for?(topic)
-    
+    votes.where(topic_id: topic.id).exists?    
   end
 end
